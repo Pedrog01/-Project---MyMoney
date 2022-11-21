b64d9f03e7b0f3310db83e9b77de61628266a8ec
Pedro <pedro7109.j@gmail.com>
2022-11-21 10:39:40 -0300

    Middleware Query Parser Int

--- src/config/server.js
+++ src/config/server.js
@@ -4,10 +4,12 @@ const bodyParse = require('body-parser');
 const express = require('express');
 const server = express();
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParse.urlencoded({extended: true}))
 server.use(bodyParse.json())
 server.use(allowCors)
+server.use(queryParser())
 
 
 server.listen(port, function () {
