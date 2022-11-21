a26c3dbaf77546e393170e8f0fa3431de09d9c73
Pedro <pedro7109.j@gmail.com>
2022-11-21 09:42:43 -0300

    Getting the Payment Summary

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -6,33 +6,32 @@ BillingCycle.updateOptions({new:true, runValidators: true})
 BillingCycle.route('count', (req, res, next) =>{
     BillingCycle.Count((error,value)=>{
             if(error) {
-    
+
                 res.status(500).json({errors: [error]})
-    
+
             } else {
-    
+
                 res.json({value})
-    
+
             }
-    
+
         })
-        
+
     })
 
-BillingCycle.route('get', (req, res, next) => {
-
-    BillingCycle.find({}, (err, docs) => {
-
-        if(!err) {
-
-            res.json(docs)
-
-        } else {
-
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{ 
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
+    }, { 
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+    }, { 
+        $project: {_id: 0, credit: 1, debt: 1}
+    }], (error, result) => {
+        if(error) {
             res.status(500).json({errors: [error]})
-
+        } else {
+            res.json(result[0] || {credit: 0, debt: 0})
         }
-
     })
 })
 
