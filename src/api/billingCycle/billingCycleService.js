2b26d0fd6265f0b6d4216993006bf47e20ef4ff3
Pedro <pedro7109.j@gmail.com>
2022-11-21 10:18:42 -0300

    Handling Error Messages

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,18 +1,36 @@
 const BillingCycle = require('./billingCycle');
+const errorHandler = require('../common/errorHandler')
+
 
 BillingCycle.methods(['get','post','put','delete']);
 BillingCycle.updateOptions({new:true, runValidators: true})
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
-BillingCycle.route('count', (req, res, next) =>{
+
+BillingCycle.route('get', (req, res, next) => {
+
+    BillingCycle.find({}, (err, docs) => {
+
+        if(!err) {
+
+            res.json(docs)
+
+        } else {
+
+            res.status(500).json({errors: [error]})
+
+        }
+
+    })
+
+})
+
+BillingCycle.route('count', (req, res, next) =>{ 
     BillingCycle.Count((error,value)=>{
             if(error) {
-
                 res.status(500).json({errors: [error]})
-
             } else {
-
                 res.json({value})
-
             }
 
         })
