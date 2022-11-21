07724141365a171e8dc05fad2cbf9c47a4dbe5a5
Pedro <pedro7109.j@gmail.com>
2022-11-21 10:28:56 -0300

    Enabling CORS

--- src/config/server.js
+++ src/config/server.js
@@ -3,9 +3,12 @@ const port = 3000
 const bodyParse = require('body-parser');
 const express = require('express');
 const server = express();
+const allowCors = require('./cors')
 
 server.use(bodyParse.urlencoded({extended: true}))
-server.use(bodyParse.json());
+server.use(bodyParse.json())
+server.use(allowCors)
+
 
 server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`)
