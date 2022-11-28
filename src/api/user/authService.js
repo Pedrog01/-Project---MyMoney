e7fc19399a8491724c2d82d07429d41f4cdd0ecf
Pedro <pedro7109.j@gmail.com>
2022-11-28 08:26:22 -0300

    adding validation token

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('./user')
 const env = require('../../.env')
+const user = require('./user')
 
 const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
@@ -11,6 +12,32 @@ const sendErrorsFromDB = (res, dbErrors) => {
     const errors = []
     _.forIn(dbErrors.errors, error => errors.push(error.message))
     return res.status(400).json({errors})
-    }
+}
 
+const login =(req,res,next) => {
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    
+    User.findOne({email}, (res,err) =>{
+        if(err){
+            return sendErrorsFromDB(res,err)
+        }else if(user && bcrypt.compareSync(password.user.password)){
+            const token = jwt.sign(user, env.authSecret,{
+                expiresIn: '1 Day'
+            })
+            const {name, email} = user
+            res.json({name, email, token})
+        }else{
+            res.status(400).send({errors: ['Usuário/Senha Invalidos']})
+        }
+    })
+}
+
+const validateToken = (req, res, next) => {
+    const token = req.body.token
+        
+    jwt.verify(token, env.authSecret, function(err, decoded) {
+        return res.status(200).send({valid: !err})
+    })
+}
 
